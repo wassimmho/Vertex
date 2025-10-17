b355f95c55433c4f85b380ee443925bf4e296041
wassimmho <wassim.mho000@gmail.com>
2025-10-17 11:23:47 +0100

    data analytics

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,9 +7,20 @@ import Contribute from './Components/Contribute/Contribute.jsx'
 import Navbar from './Components/Navbar/Navbar.jsx'
 import AddQuestion from './Components/AddQuestion/AddQuestion.jsx'
 import Footerdoc from "./Components/Footerdoc/Footerdoc";
+import { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+import ReactGA from 'react-ga4';
 import './App.css'
 
 function App() {
+  
+  ReactGA.initialize('G-W62G1T5RR0');
+  const location = useLocation();
+  useEffect(() => {
+    ReactGA.send({ hitType: "pageview", page: location.pathname });
+  }, [location]);
+
+
   return (
     //<AddQuestion/>
      <BrowserRouter>
